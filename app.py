c9dce37db2bf7dd9d6a5a50bf3caa390919123ae
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2024-10-06 23:31:19 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,83 @@
+import streamlit as st
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=api_key)
+
+def load_data_source(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context. 
+    If the answer is not in the provided context, just say, "answer is not available in the context".
+    
+    Context:\n {context}\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+    
+    response = chain(
+        {"input_documents": docs, "question": user_question},
+        return_only_outputs=True
+    )
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+def main():
+    st.set_page_config("Chat Data Source")
+    st.header("IHRD BOT")
+
+    # Load the data source from a text file
+    file_path = "data.txt"  # Specify your text file name here
+    data_source = load_data_source(file_path)
+
+    # Initialize the vector store on startup
+    if not os.path.exists("faiss_index.index"):
+        st.info("Processing data source...")
+        text_chunks = get_text_chunks(data_source)
+        get_vector_store(text_chunks)
+        # st.success("Data source processed and vector store created.")
+
+    user_question = st.text_input("Have a query on IHRD? ask")
+
+    if user_question:
+        user_input(user_question)
+
+if __name__ == "__main__":
+    main()
